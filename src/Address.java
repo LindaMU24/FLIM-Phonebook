fbb405a304885ad974cb75a4aa081ed0af0ef0fe
“madeleineMU24” <3tomu24.thunma@folkuniversitetet.nu>
2024-11-28 15:17:52 +0100

    added setter

--- src/Address.java
+++ src/Address.java
@@ -21,30 +21,26 @@ public class Address {
     }
 
     public void setCity(String city) {
-        this.city = city;
-    }
+        this.city = city;}
 
     public int getZipCode() {
         return zipCode;
     }
 
     public void setZipCode(int zipCode) {
-        this.zipCode = zipCode;
-    }
+        this.zipCode = zipCode;}
 
     public String getStreetName() {
         return streetName;
     }
 
     public void setStreetName(String streetName) {
-        this.streetName = streetName;
-    }
+        this.streetName = streetName;}
 
     public String getStreetNumber() {
         return streetNumber;
     }
 
     public void setStreetNumber(String streetNumber) {
-        this.streetNumber = streetNumber;
-    }
+        this.streetNumber = streetNumber;}
 }
