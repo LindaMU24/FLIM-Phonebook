859ffd37f874a4fa2ca37481c57823f05e524518
Ivana Lovric <ivana.lovric1314@gmail.com>
2024-11-29 16:23:11 +0100

    refactored code in InputHandler

--- src/InputHandler.java
+++ src/InputHandler.java
@@ -2,27 +2,7 @@
 import java.util.Scanner;
 
 public class InputHandler {
-
-
-    private static Scanner scanner = new Scanner(System.in);
-
-    // menu which user is admin or guest
-    public int readWhichUser() {
-        int answer = scanner.nextInt();
-        if (answer == 1) {                                   // can just read file (search)
-            System.out.println("Welcome guest!\n");
-            return 1;
-        }
-        if (answer == 2) {
-            System.out.println("Welcome admin!\n");
-            System.out.println("Menu for admin! \n");    // can read (search) and write (update, delete...)file
-            return 2;
-        } else {
-            System.out.println("Invalid input! You are guest!");   // default is guest
-        }
-
-        return answer;
-    }
+    public static Scanner scanner = new Scanner(System.in);
 
     public static int getIntInRange(int min, int max) {
         while (true) {
