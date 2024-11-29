e833da911b76bc6e5c3f336f67432e696d21175f
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2024-11-29 10:54:42 +0100

    added User and AdminUser

---
+++ src/AdminUser.java
@@ -0,0 +1,19 @@
+public class AdminUser extends User {
+
+    private String userName;
+    private String password;
+
+    public AdminUser(String name, String userName, String password) {
+        super(name);
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
