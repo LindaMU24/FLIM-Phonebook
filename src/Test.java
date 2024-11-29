ed92d4892fc4057e12c8f5683b1703c6e37ee79b
Ivana Lovric <ivana.lovric1314@gmail.com>
2024-11-29 09:31:02 +0100

    add new print line

--- src/Test.java
+++ src/Test.java
@@ -2,13 +2,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Test {
-    public static void testDefaultConstructorOfProfile(){
+    public static void testDefaultConstructorOfProfile() {
         List<Profile> profiles = new ArrayList<>();
         profiles.add(new Profile());
     }
 
     public static void main(String[] args) {
         testDefaultConstructorOfProfile();
+        System.out.println("This works!");
+        System.out.println("This works!");
         System.out.println("This works!");
     }
 }
