3022e4dc06aeb8eccac2fc333ce8fdcb1ce7361c
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2024-11-29 14:45:32 +0100

    added menus and a while loop for search menu

--- src/Menu.java
+++ src/Menu.java
@@ -3,8 +3,6 @@ import java.util.List;
 
 public abstract class Menu {
 
-
-
     public static void mainMenu(){
         boolean running = true;
         while(running){
@@ -73,26 +71,38 @@ public abstract class Menu {
             System.out.println("##   Welcome to the phonebook!   ##");
             System.out.println("##   Logged in as administrator   ##");
             System.out.println("What do you want to do?\n");
-            System.out.println(". Search");
-            System.out.println("1. Add profile");
+            System.out.println("1. Search");
+            System.out.println("2. Add profile");
             System.out.println(". Remove profile");
             System.out.println(". Update profile");
             System.out.println("0. Quit to main menu");
             int choice = InputHandler.getIntInRange(0,1);
             switch(choice) {
-                case 1 -> ProfileHandler.addProfile();
+                case 1 -> searchMenu();
+                case 2 -> ProfileHandler.addProfile();
                 case 0 -> running = false;
             }
         }
     }
 
     public static void searchMenu(){
-        System.out.println("##   Search   ##");
-        System.out.println("Please select search method: \n");
-        System.out.println("1. By first name");
-        System.out.println("2. By last name");
-        System.out.println("3. By address");
-        System.out.println("4. Free search");
+        boolean running = true;
+        while(running) {
+            System.out.println("##   Search   ##");
+            System.out.println("Please select search method: \n");
+            System.out.println("1. By first name");
+            System.out.println(". By last name");
+            System.out.println(". By address");
+            System.out.println(". Free search");
+            int choice = InputHandler.getIntInRange(0,1);
+            switch(choice) {
+                case 1 -> {
+                    System.out.println("Please enter the first name of the person you wish to find: ");
+                    String firstName = InputHandler.getToString();
+                    SearchProfile.searchFirstName(firstName);
+                }
+                case 0 -> running = false;
+            }
+        }
     }
-
 }
