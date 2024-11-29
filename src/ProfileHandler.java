365e6a5ee2515ee13c3eb0aff83b1aacd5eaa248
“madeleineMU24” <3tomu24.thunma@folkuniversitetet.nu>
2024-11-29 13:46:31 +0100

    added addMoreNumbers

--- src/ProfileHandler.java
+++ src/ProfileHandler.java
@@ -6,6 +6,7 @@ public class ProfileHandler {
     Scanner scanner = new Scanner(System.in);
     List<Profile> profileList = new ArrayList<>();
     List<PhoneNumber> phoneNumbers = new ArrayList<>();
+
 public void addProfile(Scanner scanner){
 
     System.out.println("Enter the firstname of the person: ");
@@ -14,11 +15,9 @@ public void addProfile(Scanner scanner){
     String lastName = scanner.nextLine();
     System.out.println("Enter the age of the person: ");
     int age = scanner.nextInt();
-    System.out.println("Enter the phonenumber: ");
-    String phoneNumber = scanner.nextLine();
-    System.out.println("Enter type of number (mobile, home, work): ");
-    String type = scanner.nextLine();
-    phoneNumbers.add(new PhoneNumber(phoneNumber, type));
+
+addPhoneNumbers(scanner);
+
     System.out.println("Enter the city: ");
     String city = scanner.nextLine();
     System.out.println("Enter the zipcode: ");
@@ -34,5 +33,31 @@ public void addProfile(Scanner scanner){
 
     public List<Profile> getProfileList() {
         return profileList;
+    }
+
+    public void addPhoneNumbers(Scanner scanner){
+    boolean addMoreNumbers = true;
+    while (addMoreNumbers){
+        System.out.println("Enter the phonenumber: ");
+        String phoneNumber = scanner.nextLine();
+        System.out.println("Enter type of number (mobile, home, work): ");
+        String type = scanner.nextLine();
+        phoneNumbers.add(new PhoneNumber(phoneNumber, type));
+
+        System.out.println("Do you want to add a another number? Y/N");
+        String response = scanner.nextLine();
+
+        if (!response.equalsIgnoreCase("Y")){
+            addMoreNumbers = false;}
+    }
+
+    }
+
+    public void updateProfile(){
+
+    }
+
+    public void deleteProfile(){
+
     }
 }
