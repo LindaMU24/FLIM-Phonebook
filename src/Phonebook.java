eb3b2a617d25af648112996cde3ed96e15647a2b
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2024-12-02 10:01:52 +0100

    Singleton Phonebook. Updated Main, ProfileHandler, SearchProfile with calls to list in Phonebook

--- src/Phonebook.java
+++ src/Phonebook.java
@@ -2,26 +2,49 @@ import java.util.ArrayList;
 import java.util.List;
 public class Phonebook {
 
+
+    private static Phonebook instance;
     private static List<Profile> profiles = new ArrayList<>();
-    public static void run(){
 
-        addInitialProfilesToList();
+    private Phonebook(){}
 
+    public static Phonebook getInstance(){
+        if( instance == null){
+            instance = new Phonebook();
+            addInitialProfilesToList();
+        }
+        return instance;
     }
 
-    static List<Profile> getProfiles(){return profiles;} //I need that for print list
 
+    //I need that for print list
+    public static List<Profile> getProfiles(){
+        return profiles;
+    }
+
+    public static void addProfile(Profile profile){
+        profiles.add(profile);
+    }
+
+    public static void removeProfile(Profile profile){
+        profiles.remove(profile);
+    }
+
+
+
+
+    // This is only used to populate the list of contacts at the start of the program.
     public static void addInitialProfilesToList(){
         List<PhoneNumber> phoneNumbers = new ArrayList<>();
         phoneNumbers.add(new PhoneNumber("0123456789", "work"));
         phoneNumbers.add(new PhoneNumber("986532875421", "home"));
-        profiles.add(new Profile("Fredrik", "Andersson", 38, phoneNumbers,new Address("Väderstad", 59636, "Folkungavägen", "1B")));
-        profiles.add(new Profile("Bill", "Andersson", 38, phoneNumbers,new Address("Väderstad", 59636, "Folkungavägen", "1B")));
-        profiles.add(new Profile("Liz", "Andersson", 38, phoneNumbers,new Address("Väderstad", 59636, "Folkungavägen", "1B")));
-        profiles.add(new Profile("Bob", "Andersson", 38, phoneNumbers,new Address("Väderstad", 59636, "Folkungavägen", "1B")));
-        profiles.add(new Profile("Ned", "Andersson", 38, phoneNumbers,new Address("Väderstad", 59636, "Folkungavägen", "1B")));
-        profiles.add(new Profile("Beth", "Andersson", 38, phoneNumbers,new Address("Väderstad", 59636, "Folkungavägen", "1B")));
-        profiles.add(new Profile("Zid", "Andersson", 38, phoneNumbers,new Address("Väderstad", 59636, "Folkungavägen", "1B")));
+        profiles.add(new Profile("Lisa", "Svensson", 38, phoneNumbers,new Address("Stockholm", 12345, "Blåbärsvägen", "12")));
+        profiles.add(new Profile("Erik", "Johansson", 38, phoneNumbers,new Address("Göteborg", 54321, "Lingonstigen", "8")));
+        profiles.add(new Profile("Anna", "Nilsson", 38, phoneNumbers,new Address("Malmö", 67890, "Hallonvägen", "5")));
+        profiles.add(new Profile("Johan", "Andersson", 38, phoneNumbers,new Address("Uppsala", 11223, "Smultronstigen", "22")));
+        profiles.add(new Profile("Karin", "Larsson", 38, phoneNumbers,new Address("Linköping", 33445, "Körsbärsvägen", "14")));
+        profiles.add(new Profile("Fredrik", "Olsson", 38, phoneNumbers,new Address("Västerås", 55667, "Rönnbärsvägen", "7")));
+        profiles.add(new Profile("Maria", "Persson", 38, phoneNumbers,new Address("Örebro", 77889, "Äppelstigen", "9")));
     }
 
 }
